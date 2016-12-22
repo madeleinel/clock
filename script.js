db7bb93f20f9b03ccb59831ba8c713a54f414c5b
Madeleine Linder <madeleinelinder@hotmail.com>
2016-12-22 15:17:35 +0000

    Updated JS

--- script.js
+++ script.js
@@ -2,25 +2,25 @@
 
 // set up a constant / variable for each clock hand
 
-const secondHand = document.querySelector('#second-hand');
-const minsHand = document.querySelector('#minute-hand');
-const hourHand = document.querySelector('#hour-hand');
+var secondHand = document.querySelector("#second-hand");
+var minsHand = document.querySelector("#minute-hand");
+var hourHand = document.querySelector("#hour-hand");
 
 function setDate () {
-  const now = new Date();
+  var now = new Date();
 
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  var seconds = now.getSeconds();
+  var secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + 90;
+  var mins = now.getMinutes();
+  var minsDegrees = ((mins / 60) * 360) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   // set up the hour hand so it moves each minute, to enable it to move between the full hour marks
-  const hour = now.getHours();
-  const currHour = hour + (mins / 60);
-  const hourDegrees = ((currHour / 12) * 360) + 90;
+  var hour = now.getHours();
+  var currHour = hour + (mins / 60);
+  var hourDegrees = ((currHour / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
   console.log("seconds =" + seconds);
@@ -30,13 +30,15 @@ function setDate () {
 
 setInterval(setDate, 1000);
 
+setDate();
+
 // setting up styling functions
 
-const inputs = document.querySelectorAll('.controls input');
+var inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
   document.documentElement.style.setProperty(`--${this.name}`, this.value);
 }
 
 // when 'hear' changed value of each input type >> run the handleUpdate function
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
